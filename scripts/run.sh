d80f6fe5ab72a75138720708d67c432a5733f69a
heheda <zhangch99@outlook.com>
2020-08-20 21:44:05 +0800

    benchmark script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-task=supremacy_28
-source ../scripts/init.sh -DUSE_GROUP=off -DSHOW_SUMMARY=off -DSHOW_SCHEDULE=off
-./main ../tests/input/$task.qasm 2>&1 | tee log1
-source ../scripts/init.sh -DUSE_GROUP=on -DSHOW_SUMMARY=on -DSHOW_SCHEDULE=on
-./main ../tests/input/$task.qasm 2>&1 | tee log2
-diff log1 log2
+source ../scripts/init.sh -DUSE_GROUP=on -DSHOW_SUMMARY=on -DSHOW_SCHEDULE=off -DMICRO_BENCH=on
+./local-single 2>&1 | tee log3
