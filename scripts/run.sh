a29254177626637b0cd70d438f3c04ebfcd9aa38
leenldk <3348263546@qq.com>
2021-01-20 11:13:12 +0800

    adjusted .sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ../scripts/init.sh -DBACKEND=group -DSHOW_SUMMARY=on -DSHOW_SCHEDULE=on -DMICRO_BENCH=on -DUSE_DOUBLE=on -DDISABLE_ASSERT=off
-CUDA_VISIBLE_DEVICES=0,1,2,3 ./main ../tests/input/qft_28.qasm
+CUDA_VISIBLE_DEVICES=0 ./main ../tests/input/qft_28.qasm
