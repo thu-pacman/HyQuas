991cdbd479fab248129c43a7369417c7b0d24419
heheda <zhangch99@outlook.com>
2021-01-30 11:38:06 +0800

    bench backend

--- scripts/bench_backend.sh
+++ scripts/bench_backend.sh
@@ -2,19 +2,22 @@
 export CUDA_VISIBLE_DEVICES=0
 head=../build/logs/`date +%Y%m%d-%H%M%S`
 
+source ../scripts/init.sh -DBACKEND=group -DSHOW_SUMMARY=on -DSHOW_SCHEDULE=off -DMICRO_BENCH=on -DUSE_DOUBLE=on -DEVALUATOR_PREPROCESS=on
+CUDA_VISIBLE_DEVICES=0 ../build/process
+
 name=$head-group
 mkdir -p $name
-./check_wrapper.sh $name -DBACKEND=group -DSHOW_SUMMARY=on -DSHOW_SCHEDULE=off -DUSE_DOUBLE=on -D COALESCE=0 2>&1 | tee $name/std.out
+./check_wrapper.sh $name -DBACKEND=group -DSHOW_SUMMARY=on -DSHOW_SCHEDULE=off -DUSE_DOUBLE=on 2>&1 | tee $name/std.out
 name1=$name
 
 name=$head-blas
 mkdir -p $name
-./check_wrapper.sh $name -DBACKEND=group -DSHOW_SUMMARY=on -DSHOW_SCHEDULE=off -DUSE_DOUBLE=on -DCOALESCE=1 2>&1 | tee $name/std.out
+./check_wrapper.sh $name -DBACKEND=blas -DSHOW_SUMMARY=on -DSHOW_SCHEDULE=off -DUSE_DOUBLE=on 2>&1 | tee $name/std.out
 name2=$name
 
 name=$head-mix
 mkdir -p $name
-./check_wrapper.sh $name -DBACKEND=group -DSHOW_SUMMARY=on -DSHOW_SCHEDULE=off -DUSE_DOUBLE=on -DCOALESCE=2 2>&1 | tee $name/std.out
+./check_wrapper.sh $name -DBACKEND=mix -DSHOW_SUMMARY=on -DSHOW_SCHEDULE=off -DUSE_DOUBLE=on -DEVALUATOR_PREPROCESS=on 2>&1 | tee $name/std.out
 name3=$name
 
 grep -r "Time Cost" $head-*/*.log
